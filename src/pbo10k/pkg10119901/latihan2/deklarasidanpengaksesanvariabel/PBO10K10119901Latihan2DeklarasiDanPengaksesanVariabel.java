1d2a1e800eb1cae11c92b63a8fdc356ed4de8536
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 22:58:02 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan2/deklarasidanpengaksesanvariabel/PBO10K10119901Latihan2DeklarasiDanPengaksesanVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan2.deklarasidanpengaksesanvariabel;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Deklarasi Dan Pengaksesan Variabel
+ *
+ */
+public class PBO10K10119901Latihan2DeklarasiDanPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt        = 78;
+        nilaiLogika     = false;
+        nilaiKarakter   = 'D';
+        
+        System.out.println();
+        System.out.println("Isi variabel nilai \t = " + nilaiInt);
+        System.out.println("Isi variabel PHI \t = " + PHI);
+        System.out.println("Isi variabel logika \t = " + nilaiLogika);
+        System.out.println("Isi variabel karakter \t = " + nilaiKarakter);
+    }
+    
+}
